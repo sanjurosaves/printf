8b3c4530c36792a80e74c87a6f9568a1f07480f8
Alex Allen <325@holbertonschool.com>
2018-02-25 02:18:00 +0000

    defining structure for data types/format specifiers

--- holberton.h
+++ holberton.h
@@ -1,6 +1,22 @@
 #ifndef HOLBERTON_H_
 #define HOLBERTON_H_
 
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct type - incoming data type/format
+ * @fmt_spec: pointer to format specifier string
+ * @f: associated function name
+ */
+typedef struct type
+{
+	char *fmt_spec;
+	void (*f)(va_list fmt_spec);
+} data_type;
+
 int _printf(const char *format, ...);
 
 #endif
